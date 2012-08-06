7a5db220be84a66d08e7e3f214645a92168ca4d1
dreamlx <dreamlinx@gmail.com>
2012-08-06 14:22:51 +0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShanghaiTonightGuide::Application.config.secret_token = '29681aeb5fe036233cd37a94f1cd39a1c01999600530c7664e2e12f975028986534b6f1c83abae0a79f8bcbbebff571e64f92f414ad38b659252e11a73d48706'
