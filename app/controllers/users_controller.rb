2d8f8108f2d4434da9d008adbe2ad5c02ac5fc48
dreamlx <dreamlinx@gmail.com>
2012-08-07 17:43:23 +0800

    add json support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,20 @@ class UsersController < ApplicationController
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
     @users = User.all
+    respond_to do |format|   
+      format.html   
+      format.xml { render :xml => @users }   
+      format.json { render :text => @users.to_json }   
+    end
   end
 
   def show
     @user = User.find(params[:id])
+    respond_to do |format|   
+      format.html   
+      format.xml { render :xml => @user }   
+      format.json { render :text => @user.to_json }   
+    end
   end
 
 end
